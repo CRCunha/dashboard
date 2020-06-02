35abcf9c541a6e11b804b81de068658e22dd054d
cristian <contato@cristiancunha.me>
2020-06-01 21:34:08 -0300

    Menu

--- src/components/navBar/index.js
+++ src/components/navBar/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "./navBar.css";
 import Logo from "../../assets/logo.png";
+import ShowChartIcon from "@material-ui/icons/ShowChart";
+import WebAssetOutlinedIcon from "@material-ui/icons/WebAssetOutlined";
+import TurnedInNotOutlinedIcon from "@material-ui/icons/TurnedInNotOutlined";
+import DraftsOutlinedIcon from "@material-ui/icons/DraftsOutlined";
 
 const NavBar = () => {
   return (
@@ -8,6 +12,22 @@ const NavBar = () => {
       <div className="container">
         <div className="logo">
           <img src={Logo} alt="Logo" />
+        </div>
+        <div className="menu">
+          <div className="container">
+            <div className="menuIten">
+              <DraftsOutlinedIcon />
+            </div>
+            <div className="menuIten">
+              <ShowChartIcon />
+            </div>
+            <div className="menuIten">
+              <WebAssetOutlinedIcon />
+            </div>
+            <div className="menuIten">
+              <TurnedInNotOutlinedIcon />
+            </div>
+          </div>
         </div>
       </div>
     </nav>
