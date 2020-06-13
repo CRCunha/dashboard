1dd60d4b297393b368d88a3fc8ff5aaf37e4ee8e
cristian <contato@cristiancunha.me>
2020-06-13 07:11:03 -0300

    title on fullwidth charts

--- src/components/bumpChart/index.js
+++ src/components/bumpChart/index.js
@@ -7,47 +7,50 @@ import { ResponsiveBump } from "@nivo/bump";
 const BumpChart = () => {
   return (
     <div className="bumpChart">
-      <ResponsiveBump
-        data={data}
-        margin={{ top: 40, right: 100, bottom: 40, left: 60 }}
-        colors={ListColors}
-        lineWidth={3}
-        activeLineWidth={6}
-        inactiveLineWidth={3}
-        inactiveOpacity={0.15}
-        pointSize={10}
-        activePointSize={16}
-        inactivePointSize={0}
-        pointColor={{ theme: "background" }}
-        pointBorderWidth={3}
-        activePointBorderWidth={3}
-        pointBorderColor={{ from: "serie.color" }}
-        axisTop={{
-          tickSize: 5,
-          tickPadding: 5,
-          tickRotation: 0,
-          legend: "",
-          legendPosition: "middle",
-          legendOffset: -36,
-        }}
-        axisRight={null}
-        axisBottom={{
-          tickSize: 5,
-          tickPadding: 5,
-          tickRotation: 0,
-          legend: "",
-          legendPosition: "middle",
-          legendOffset: 32,
-        }}
-        axisLeft={{
-          tickSize: 5,
-          tickPadding: 5,
-          tickRotation: 0,
-          legend: "ranking",
-          legendPosition: "middle",
-          legendOffset: -40,
-        }}
-      />
+      <div className="title">BumpChart</div>
+      <div className="responsiveContainer">
+        <ResponsiveBump
+          data={data}
+          margin={{ top: 40, right: 100, bottom: 40, left: 60 }}
+          colors={ListColors}
+          lineWidth={3}
+          activeLineWidth={6}
+          inactiveLineWidth={3}
+          inactiveOpacity={0.15}
+          pointSize={10}
+          activePointSize={16}
+          inactivePointSize={0}
+          pointColor={{ theme: "background" }}
+          pointBorderWidth={3}
+          activePointBorderWidth={3}
+          pointBorderColor={{ from: "serie.color" }}
+          axisTop={{
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: "",
+            legendPosition: "middle",
+            legendOffset: -36,
+          }}
+          axisRight={null}
+          axisBottom={{
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: "",
+            legendPosition: "middle",
+            legendOffset: 32,
+          }}
+          axisLeft={{
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: "ranking",
+            legendPosition: "middle",
+            legendOffset: -40,
+          }}
+        />
+      </div>
     </div>
   );
 };
--- src/components/calendarChart/index.js
+++ src/components/calendarChart/index.js
@@ -7,30 +7,33 @@ import { ListColors } from "../colorCharts.js";
 const CaledarChart = () => {
   return (
     <div className="calendarChart">
-      <ResponsiveCalendar
-        data={data}
-        from="2017-01-2"
-        to="2017-12-30"
-        emptyColor="#eeeeee"
-        colors={ListColors}
-        margin={{ top: 40, right: 40, bottom: 40, left: 40 }}
-        yearSpacing={40}
-        monthBorderColor="#ffffff"
-        dayBorderWidth={2}
-        dayBorderColor="#ffffff"
-        legends={[
-          {
-            anchor: "bottom-right",
-            direction: "row",
-            translateY: 36,
-            itemCount: 4,
-            itemWidth: 42,
-            itemHeight: 36,
-            itemsSpacing: 14,
-            itemDirection: "right-to-left",
-          },
-        ]}
-      />
+      <div className="title">CalendarChart</div>
+      <div className="responsiveContainer">
+        <ResponsiveCalendar
+          data={data}
+          from="2017-01-2"
+          to="2017-12-30"
+          emptyColor="#eeeeee"
+          colors={ListColors}
+          margin={{ top: 0, right: 40, bottom: 40, left: 40 }}
+          yearSpacing={40}
+          monthBorderColor="#ffffff"
+          dayBorderWidth={2}
+          dayBorderColor="#ffffff"
+          legends={[
+            {
+              anchor: "bottom-right",
+              direction: "row",
+              translateY: 36,
+              itemCount: 4,
+              itemWidth: 42,
+              itemHeight: 36,
+              itemsSpacing: 14,
+              itemDirection: "right-to-left",
+            },
+          ]}
+        />
+      </div>
     </div>
   );
 };
